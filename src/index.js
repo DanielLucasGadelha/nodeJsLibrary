b303fbdbfeb3d9414949165f66b3a53a9308754a
Daniel Lucas <danielgadelha.abreu@gmail.com>
2025-12-09 09:51:20 -0300

    refactor: update file reading to process word occurrences

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ const fs = require('fs');
 const pathFile = process.argv; 
 const link = pathFile[2];
 
-fs.readFile(link, 'utf8', (err, data) => {
-        console.log(data);
+fs.readFile(link, 'utf8', (err, text) => {
+        verifyTwicedWords(text);
 });
 
+// Creat an array with words
+//count the ocorrences of each word
+//make an object with the results
 
+function verifyTwicedWords(text) {
+        const wordsArray = text.split(' ');
+        const results = {};
+        wordsArray.forEach(word => {
+                results[word] = (results[word] || 0) + 1;
+        })
+
+
+}
