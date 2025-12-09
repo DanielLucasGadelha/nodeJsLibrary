b6f00c37d3ae9aea64e48f8381e3e2a056011c4f
Daniel Lucas <danielgadelha.abreu@gmail.com>
2025-12-09 16:29:28 -0300

    refactor: restructure text processing to handle paragraphs and word occurrences

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ const pathFile = process.argv;
 const link = pathFile[2];
 
 fs.readFile(link, 'utf8', (err, text) => {
-        verifyTwicedWords(text);
+        breakInToParagraphs(text);
+        //verifyTwicedWords(text);
 });
 
 // Creat an array with words
 //count the ocorrences of each word
 //make an object with the results
+
+function breakInToParagraphs(text) {
+       const paragraphs = text
+       .toLowerCase()
+       .split('\n');
+       const count = paragraphs.map((paragraph) => {
+        return verifyTwicedWords(paragraph);
+       })
+       console.log(count)
+}
 
 function verifyTwicedWords(text) {
         const wordsArray = text.split(' ');
@@ -17,6 +28,5 @@ function verifyTwicedWords(text) {
         wordsArray.forEach(word => {
                 results[word] = (results[word] || 0) + 1;
         })
-
-
+        return results;
 }
