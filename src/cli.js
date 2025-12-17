1ac0a373ee4e629ae23e0788c1d208222060bd49
Daniel Lucas <danielgadelha.abreu@gmail.com>
2025-12-17 11:10:51 -0300

    feat: add TypeScript typings for commander and update CLI to use commander for argument parsing
    - Added TypeScript typings for commander in `typings/esm.d.mts` and `typings/index.d.ts`.
    - Updated `package.json` to include commander as a dependency.
    - Created `package-lock.json` to lock dependencies.
    - Refactored CLI in `src/cli.js` to utilize commander for handling command-line arguments and options.
    - Enhanced error handling and output file processing in the CLI.
    - Updated output results in `resultado/resultado.txt` with new repeated words.

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,51 @@
 import fs from 'fs'
+import path from 'path';
 import dealErrors from './Erros/funcoesErro.js';
 import { countWords } from './index.js';
 import { mountOutputArchive } from './helpers.js';  
+import { Command } from 'commander';
 
-const pathFile = process.argv; 
-const link = pathFile[2];
-const addres = pathFile[3];
+const program = new Command();
 
-fs.readFile(link, 'utf8', (err, text) => {
+program
+    .version('0.0.1')
+    .option('-t, --text <string>', 'Caminho do texto a ser processado')
+    .option('-d, --destiny <string>', 'PathFile onde salva o arquivo results')
+    .action((options) => {
+        const {text, destiny} = options; 
+
+        if (!text || !destiny) {
+            console.error("erro: favor inserir caminho de origem e destino", error)
+            program.help();
+            return
+        }
+
+        const pathText = path.resolve(text);
+        const pathDestiny = path.resolve(destiny);
+
+        try{    
+            processArchive(pathText, pathDestiny);
+            console.log('Texto processado')
+        } catch(err) {
+            console.log('error ocurred', err)
+
+        }
+    });
+
+    program.parse();
+function processArchive(text, destiny) {
+fs.readFile(text, 'utf8', (err, text) => {
 try {
         if (err) throw err
         const results = countWords(text);
-        createAndSaveArchive(results, addres)
+        createAndSaveArchive(results, destiny)
 } catch(err) {
         dealErrors(err);
-}
+    }
 })
+}
+
+
 
 async function createAndSaveArchive(wordsArray, addres){
     const newArchive = `${addres}/resultado.txt`;
